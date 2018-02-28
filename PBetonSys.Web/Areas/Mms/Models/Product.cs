5e09919d19d97effe12f459db91a2b9b1d3d82d0
zjlqsyg <zjlqsyg@126.com>
2018-02-28 21:46:14 +0800

    遗漏提交的项目

---
+++ PBetonSys.Web/Areas/Mms/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using PBetonSys.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PBetonSys.Web.Areas.Mms.Models
+{
+    public class Product : ModelBase
+    {
+    }
+
+    public class ProductService : ServiceBase<Product>
+    {
+        public ProductService()
+        {
+            base.ModuleName = "Betonsys";
+        }
+    }
+}
