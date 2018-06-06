1232bac510f44358d6293067a331168cbac7fdcb
zhangdeguo1 <deguo1@eastmoney.com>
2018-06-06 21:16:23 +0800

    配比审核主界面

--- PBetonSys.Web/Areas/Mms/Controllers/ConfectControllers.cs
+++ PBetonSys.Web/Areas/Mms/Controllers/ConfectControllers.cs
@@ -26,7 +26,28 @@ namespace PBetonSys.Web.Areas.Mms.Controllers
             };
             return View(model);
         }
-        
+
+        /// <summary>
+        /// 配比审核
+        /// </summary>
+        public ActionResult Audit() 
+        {
+            var model = new
+            {
+                form = new
+                {
+                    Task_id = "",
+                    cont_id = "",
+                    projectname="",
+                    Name = "",
+                    Place = "",
+                    Provide_DateTime = "",
+                    Stong="",
+                    Fall=""
+                }
+            };
+            return View(model);
+        }
         public ActionResult ConfectList()
         {
             return View();
@@ -61,7 +82,34 @@ namespace PBetonSys.Web.Areas.Mms.Controllers
             return result;
         }
 
-    
+        public dynamic GetAuditConfectList(RequestWrapper query)
+        {
+            query.LoadSettingXmlString(@"
+                <settings defaultOrderBy='CheckDateTime'>
+                    <select>
+                        task.Provide_DateTime, task.House_id as tHous,task.Pump_vehicle, task.ContractUnit,Task.TempProject, Confect.*,Contract.projectname,SysCode.DESCRIPTION,Client.Name
+                    </select>
+                    <from>
+                        Confect
+                        join  Contract on Confect.Cont_ID=Contract.cont_id and Contract.State=1
+                        join Task on Task.Task_id=Confect.Task_id
+                        left join Client on Contract.Clinet_id=Client.Cl_ID 
+                        left join SysCode on  Confect.Hous_id=SysCode.Code and SysCode.CodeType='House_id'
+                    </from>
+                  <where defaultForAll='true' defaultCp='equal' defaultIgnoreEmpty='true' >
+                    <field name='Confect.Task_id'       cp='equal'  ></field>
+                    <field name='Confect.Cont_ID'       cp='equal'   ></field>
+                  <field name='Contract.projectname'       cp='like'   ></field>
+                  <field name='Client.Name'       cp='like'   ></field>
+                  <field name='Client.Place'       cp='like'   ></field>
+                    <field name='Task.Provide_DateTime'          cp='daterange'  ></field>
+                </where>
+                </settings>");
+            var pQuery = query.ToParamQuery();
+            var result = new TaskService().GetDynamicListWithPaging(pQuery);
+            return result;
+        }
+
 
 
         [System.Web.Http.HttpPost]
---
+++ PBetonSys.Web/Areas/Mms/ViewModels/ConfectAuditList.js
@@ -0,0 +1,37 @@
+﻿/**
+* 模块名：MMS
+* 程序名: PriceInfo.js
+**/
+
+var viewModel = function (data) {
+    var self = this;
+    this.form = ko.mapping.fromJS(data.form);
+
+    this.grid = {
+        size: { w: 4, h: 50 },
+        url: '/api/Mms/Confect/GetAuditConfectList',
+        pagination: true,
+        pageSize: 20
+    };
+
+    //this.grid.queryParams(data.form);
+
+    this.searchClick = function () {
+        var param = ko.toJS(this.form);
+        self.grid.queryParams(param);
+    };
+
+    this.clearClick = function () {
+        window.location.reload();
+    };
+
+    this.refreshClick = function () {
+        window.location.reload();
+    };
+
+
+    this.grid.onDblClickRow = function ()
+    {
+
+    };
+};
